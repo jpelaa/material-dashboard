b0080836b1ab6a5007e3651d9c0c2b2d5009f221
Jayaprakash Elavarasan <Jayaprakash.Elavarasan@jumeirah.com>
2022-08-01 13:51:29 +0530

    cor resolve try

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'https://servicesuat.jumeirah.com/:path*',
+			},
+		];
+	},
+});
